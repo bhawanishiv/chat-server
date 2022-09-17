4231b1cd975094ed3f3e6f589b2cc2e3abf0a3f6
Bhawani Shankar Bharti <bhawanishiv@gmail.com>
2022-09-18 04:10:20 +0530

    Add test cases for search users, add/remove a member in a group

--- tests/groups/members.groups.spec.js
+++ tests/groups/members.groups.spec.js
@@ -26,10 +26,16 @@ const generateName = (params = {}) =>
     ...params,
   });
 
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
 describe('Group Members', () => {
-  const groupName = generateName();
+  const groupName = generateName({ name: 20 });
   let signInUser;
+  let anotherUser;
   let createdGroup;
+  let searchResult = [];
+  let searchedUser;
+  let addedMember;
 
   before(async () => {
     const userRes = await userAgent.post('/auth/v1/login').send(USER_CREDS);
@@ -54,9 +60,88 @@ describe('Group Members', () => {
     createdGroup = res.body.group;
   });
 
-  // describe('Search a member', async () => {});
+  before(async () => {
+    const anotherUserRes = await anotherUserAgent
+      .post('/auth/v1/login')
+      .send(ANOTHER_USER_CREDS);
 
-  // describe('Add a member', async () => {});
+    anotherUser = anotherUserRes.body;
 
-  // describe('Delete a member', async () => {});
+    anotherUserAgent.set(
+      'Authorization',
+      `${anotherUser.tokenType} ${anotherUser.accessToken}`
+    );
+  });
+
+  describe('Search users', async () => {
+    it('Should successfully make search users API call', async () => {
+      const res = await userAgent
+        .post('/users/v1/search')
+        .send({ keyword: 'red', limit: 10, skip: 0 })
+        .expect(200);
+
+      expect(res.body.count).to.be.an('number');
+      expect(res.body.users).to.be.an('array');
+
+      searchResult = res.body.users;
+    });
+  });
+
+  describe('Add a member', async () => {
+    before(async () => {
+      const randomIndex = getRandomInt(searchResult.length - 1);
+      searchedUser = searchResult[randomIndex];
+    });
+
+    it('Should successfully add a member', async () => {
+      const res = await userAgent
+        .post(`/groups/v1/${createdGroup.groupId}/members/${searchedUser.uid}`)
+        .expect(200);
+
+      expect(res.body.member).to.be.an('object');
+      expect(res.body.member.memberId).to.be.an('string');
+      expect(res.body.group).to.be.an('object');
+      expect(res.body.group.groupId).to.be.equal(createdGroup.groupId);
+      expect(res.body.addedBy).to.be.an('object');
+      expect(res.body.addedBy.uid).to.be.equal(signInUser.user.uid);
+
+      addedMember = res.body.member;
+    });
+
+    it('Should fail to add a member, invalid userId', async () => {
+      await userAgent
+        .post(`/groups/v1/${createdGroup.groupId}/members/bs`)
+        .expect(400);
+    });
+
+    it("Should fail to add a member, person adding the user does\tnt belong to the group, thus hasn't access to add someone", async () => {
+      await anotherUserAgent
+        .post(`/groups/v1/${createdGroup.groupId}/members/${searchedUser.uid}`)
+        .expect(401);
+    });
+
+    it('Should fail to add a member, user is already added', async () => {
+      await userAgent
+        .post(`/groups/v1/${createdGroup.groupId}/members/${searchedUser.uid}`)
+        .expect(400);
+    });
+  });
+
+  describe('Delete a member', async () => {
+    it('Should fail to remove a member, invalid memberId', async () => {
+      await userAgent.delete(`/groups/v1/members/sf`).expect(400);
+    });
+
+    it("Should fail to remove a member, user doesn't belong to member's group", async () => {
+      const res = await anotherUserAgent
+        .delete(`/groups/v1/members/${addedMember.memberId}`)
+        .expect(401);
+    });
+
+    it('Should successfully remove a member', async () => {
+      const res = await userAgent
+        .delete(`/groups/v1/members/${addedMember.memberId}`)
+        .expect(200);
+    });
+  });
 });
